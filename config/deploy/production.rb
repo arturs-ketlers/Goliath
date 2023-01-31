3989238dacccae68da6d03c89c45648d1ae0d038
Peteris Repelis <p.r@inbox.lv>
2023-01-31 14:14:44 +0200

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,4 @@ set :rails_env, fetch(:stage)
 set :user, fetch(:application)
 set :deploy_to, "/var/www/#{fetch(:application)}"
 
-server '104.248.41.182', port: 22, user: fetch(:application), roles:  %w{web app db}, primary: true
+server '46.101.146.98', port: 22, user: fetch(:application), roles:  %w{web app db}, primary: true
