5ed240901b894724600fea666a05d6e49123c301
arturs-ketlers <22769393+arturs-ketlers@users.noreply.github.com>
2024-01-08 09:37:10 +0200

    :wrench: order teams by dotal distance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @event = Event.active
     # @team_one = Team.new('one', event_id: @event.id)
     # @team_all = Team.new('all', event_id: @event.id)
-    @teams = EventTeam.all
+    @teams = EventTeam.all.order(total_distance: :desc)
 
     @totals = @event.totals if @event.event_type == 'goliath'
     @participants_count = @event.participants.with_results.count
